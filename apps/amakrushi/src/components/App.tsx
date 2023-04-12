a2d61e3945b5504382d2a56e3f9daa87e1ea481f
Prateek Jakhar <prtkjakhar@gmail.com>
2023-04-12 19:54:49 +0530

    style: UI changes

--- apps/amakrushi/src/components/App.tsx
+++ apps/amakrushi/src/components/App.tsx
@@ -61,7 +61,7 @@ const App: NextPage = () => {
   // }, [state])
 
   return (
-    <div style={{ height: "82vh", width: "100%"}}>
+    <div style={{ height: "85vh", width: "100%"}}>
       <ChatUiWindow />
     </div>
   );
