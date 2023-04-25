62b3b679d2b057375a2a9a1e9e54283ed7b791b4
Saiyan Abhishek <74703491+geeky-abhishek@users.noreply.github.com>
2023-04-25 12:28:31 +0530

    perf: new socket method implemented

--- apps/amakrushi/src/pages/chat.tsx
+++ apps/amakrushi/src/pages/chat.tsx
@@ -3,6 +3,8 @@ import Head from 'next/head';
 import dynamic from 'next/dynamic';
 import { useLocalization } from '../hooks/useLocalization';
 import Menu from '../components/menu';
+import { useContext, useEffect } from 'react';
+import { AppContext } from '../context';
 
 const ChatUiWindow = dynamic(
   () => import('../components/PhoneView/ChatWindow/ChatUiWindow'),
@@ -11,12 +13,19 @@ const ChatUiWindow = dynamic(
 
 const Chat: NextPage = () => {
   const t = useLocalization();
-
+const context=useContext(AppContext);
+useEffect(()=>{
+  if(localStorage.getItem('phoneNumber')){
+    context?.setIsMobileAvailable(true)
+  }
+},[context])
+console.log("debug ddd:",{context})
   return (
     <>
       <Head>
         <title> {t('label.title')}</title>
       </Head>
+     
       <div
         style={{
           position: 'fixed',
