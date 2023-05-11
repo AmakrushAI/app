ad5292d2f47f7eb46f7daf79d0ea4ad824781356
Saiyan Abhishek <74703491+geeky-abhishek@users.noreply.github.com>
2023-05-11 17:23:46 +0530

    chore: testing

--- apps/amakrushi/src/pages/_app.tsx
+++ apps/amakrushi/src/pages/_app.tsx
@@ -77,17 +77,18 @@ const App = ({
 
 
   useEffect(()=>{
-    axios
-    .get(`/api/sha`)
-    .then((response) => {
-     console.warn("sha:",{response})
-    })
-    .catch((err) => {
-      console.error("sha error:",{err})
-    });
-
+    // axios
+    // .get(`/api/sha`)
+    // .then((response) => {
+    //  console.warn("sha:",{response})
+    // })
+    // .catch((err) => {
+    //   console.error("sha error:",{err})
+    // });
+    const shaFull = require('child_process').execSync(`git rev-parse HEAD`).toString().trim();
+    console.log("sha:",{shaFull})
   },[]);
-  
+
   if (launch) {
     return <LaunchPage />;
   } else {
