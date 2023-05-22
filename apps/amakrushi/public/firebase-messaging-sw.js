bf0445f678009cf6b1a087b127007f26782f5b04
Prateek Jakhar <prtkjakhar@gmail.com>
2023-05-22 21:41:47 +0530

    feat: added fcm

---
+++ apps/amakrushi/public/firebase-messaging-sw.js
@@ -0,0 +1,34 @@
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+importScripts(
+  'https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js'
+);
+
+// Set Firebase configuration, once available
+self.addEventListener('fetch', () => {
+  const urlParams = new URLSearchParams(location.search);
+  self.firebaseConfig = Object.fromEntries(urlParams);
+});
+
+// "Default" Firebase configuration (prevents errors)
+const defaultConfig = {
+  apiKey: true,
+  projectId: true,
+  messagingSenderId: true,
+  appId: true,
+};
+
+// Initialize Firebase app
+firebase.initializeApp(self.firebaseConfig || defaultConfig);
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: payload.notification.image,
+    tag: 'notification',
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
