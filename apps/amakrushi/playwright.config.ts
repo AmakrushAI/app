78f674885d65e5edf4f1a9b41c9a6864e2c7c050
Prateek Jakhar <prtkjakhar@gmail.com>
2023-06-15 18:51:56 +0530

    fix: increased timeout duration of test

--- apps/amakrushi/playwright.config.ts
+++ apps/amakrushi/playwright.config.ts
@@ -2,8 +2,7 @@ import { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
   testDir: './src/tests/e2e',
-  outputDir: './src/tests/test-results',
-  timeout: 60000
+  outputDir: './src/tests/test-results'
 };
 
 export default config;
