6792c0bad07dce948978bbc623672d5f745cab65
Prateek Jakhar <prtkjakhar@gmail.com>
2023-06-16 16:15:51 +0530

    fix: added report

--- apps/amakrushi/playwright.config.ts
+++ apps/amakrushi/playwright.config.ts
@@ -2,7 +2,7 @@ import { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
   testDir: './src/tests/e2e',
-  outputDir: './src/tests/test-results',
+  outputDir: '../../test-results',
   use: {
     video: 'on',
     baseURL: 'http://localhost:3000',
