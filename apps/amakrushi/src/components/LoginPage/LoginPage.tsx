47682d1703ee423da1ee13e330f94b157ace9ac8
Saiyan Abhishek <74703491+geeky-abhishek@users.noreply.github.com>
2023-06-28 13:49:06 +0530

    fix: no internet error for otp verify in offline

--- apps/amakrushi/src/components/LoginPage/LoginPage.tsx
+++ apps/amakrushi/src/components/LoginPage/LoginPage.tsx
@@ -1,20 +1,16 @@
-import styles from './login.module.css';
-import {
-  NumberInput,
-  NumberInputField,
-  Radio,
-  RadioGroup,
-} from '@chakra-ui/react';
-import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
-import { useLocalization } from '../../hooks/useLocalization';
-import { logEvent } from 'firebase/analytics';
-import { analytics } from '../../utils/firebase';
-import toast from 'react-hot-toast';
+import styles from "./login.module.css";
+import { NumberInput, NumberInputField, Spinner } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import { useLocalization } from "../../hooks/useLocalization";
+import { logEvent } from "firebase/analytics";
+import { analytics } from "../../utils/firebase";
+import toast from "react-hot-toast";
 const LoginPage: React.FC = () => {
   const router = useRouter();
-  const [input, setInput] = useState('');
-  const [value, setValue] = React.useState('2');
+  const [input, setInput] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [value, setValue] = React.useState("2");
   const [isButtonDisabled, setIsButtonDisabled] = useState(false);
   const t = useLocalization();
   // Setting the input value
@@ -31,33 +27,44 @@ const LoginPage: React.FC = () => {
       toast.error(`${t("message.invalid_mobile")}`);
     } else {
       setIsButtonDisabled(true);
-      fetch(
-        // `${process.env.NEXT_PUBLIC_OTP_BASE_URL}uci/sendOTP?phone=${input}`,
-        `${process.env.NEXT_PUBLIC_OTP_BASE_URL}api/sendOTP?phone=${input}`,
-        { method: 'GET' }
-      ).then((response) => {        
-        if (response.status === 200) {
-         // localStorage.setItem('phoneNumber',input)
-          router.push({ pathname: '/otp', query: { state: input } });
-        } else {
-          setIsButtonDisabled(false);
-          toast.error(`${t("message.otp_not_sent")}`);
-        }
-      })
+      if (navigator.onLine) {
+        setLoading(true);
+        fetch(
+          // `${process.env.NEXT_PUBLIC_OTP_BASE_URL}uci/sendOTP?phone=${input}`,
+          `${process.env.NEXT_PUBLIC_OTP_BASE_URL}api/sendOTP?phone=${input}`,
+          { method: "GET" }
+        )
+          .then((response) => {
+            setLoading(false);
+            if (response.status === 200) {
+              // localStorage.setItem('phoneNumber',input)
+              router.push({ pathname: "/otp", query: { state: input } });
+            } else {
+              setIsButtonDisabled(false);
+              toast.error(`${t("message.otp_not_sent")}`);
+            }
+          })
+          .catch((err) => {
+            setLoading(false);
+            toast.error(err.message);
+          });
+      } else {
+        toast.error("No Interet");
+      }
     }
   };
 
   useEffect(() => {
     //@ts-ignore
-    logEvent(analytics, 'Login_page');
+    logEvent(analytics, "Login_page");
   }, []);
 
   return (
     <div className={`${styles.main}`}>
-      <div className={styles.title}>{t('label.title')}</div>
+      <div className={styles.title}>{t("label.title")}</div>
 
       <div className={styles.body}>
-        <h1>{t('label.welcome')}</h1>
+        <h1>{t("label.welcome")}</h1>
 
         {/* <RadioGroup onChange={setValue} value={value}>
           <Radio value="1">{t("label.farmer")}</Radio>
@@ -68,7 +75,7 @@ const LoginPage: React.FC = () => {
 
         <form onSubmit={(event) => event?.preventDefault()}>
           <div className={styles.container}>
-            <NumberInput style={{ margin: '5vh auto 0px auto', width: '100%' }}>
+            <NumberInput style={{ margin: "5vh auto 0px auto", width: "100%" }}>
               <NumberInputField
                 height="45px"
                 padding="18px 16px"
@@ -78,9 +85,9 @@ const LoginPage: React.FC = () => {
                 fontWeight="400"
                 fontSize="14px"
                 placeholder={
-                  value === '1'
-                    ? 'Enter adhaar number'
-                    : t('message.enter_mobile')
+                  value === "1"
+                    ? "Enter adhaar number"
+                    : t("message.enter_mobile")
                 }
                 value={input}
                 onChange={handleNumber}
@@ -95,9 +102,19 @@ const LoginPage: React.FC = () => {
         >
           {t("message.register_message")}
         </div> */}
-            <button className={styles.submitButton} onClick={handleOTPPage} disabled={isButtonDisabled}>
-              {t('label.continue')}
-            </button>
+            {loading ? (
+              <button className={styles.submitButton}>
+                <Spinner />
+              </button>
+            ) : (
+              <button
+                className={styles.submitButton}
+                onClick={handleOTPPage}
+                disabled={isButtonDisabled}
+              >
+                {t("label.continue")}
+              </button>
+            )}
           </div>
         </form>
         {/* <div className={styles.signup}>
