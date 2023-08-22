59a814dde8fef883a8931213a66257b8dd7eb8a9
prtkjakhar <prtkjakhar@gmail.com>
2023-08-22 17:21:54 +0530

    history share and download

--- apps/amakrushi/src/components/down-time-page/index.tsx
+++ apps/amakrushi/src/components/down-time-page/index.tsx
@@ -2,30 +2,52 @@
 import Menu from '../menu';
 import styles from './index.module.css';
 import Image from 'next/image';
-import downTimeIcon from '../../assets/icons/down-time.svg';
+import downTimeIcon from '../../assets/images/downtime.png';
 import { useLocalization } from '../../hooks';
+import { useFlags } from 'flagsmith/react';
+import callIcon from '../../assets/icons/call-icon.svg';
 
 function DownTimePage() {
   const t = useLocalization();
+  const flags = useFlags(['dialer_number']);
   return (
     <div className={styles.container}>
+      <div className={styles.title}>{t('message.down_time_title')}</div>
       <div className={styles.imageContainer}>
-        <Image src={downTimeIcon} alt="downTimeIcon" layout='responsive' />
+        <Image src={downTimeIcon} alt="downTimeIcon" layout="responsive" />
       </div>
-      <span>{t("message.temporarily_down")}</span>
-      <p className={styles.miniText}>
+      <span>{t('message.temporarily_down')}</span>
+      {/* <p className={styles.miniText}>
         {t("message.temporarily_down_description")}
-      </p>
-      <button
-        type="button"
-        className={styles.backButton}
-        onClick={() => window?.location.reload()}>
-        {t("message.down_time_retry")}
-      </button>
+      </p> */}
+      <div className={styles.dialerBox}>
+        <a
+          href={`tel:${flags.dialer_number.value}`}
+          className={styles.footerTitle}>
+          <div className={styles.callIconBox}>
+            <Image src={callIcon} alt="callIcon" layout="responsive" />
+          </div>
+          <p style={{textDecoration: 'underline'}}>{t('label.call_amakrushi')}</p>
+        </a>
+      </div>
+
+      <div className={styles.buttons}>
+        <button
+          type="button"
+          className={styles.retryButton}
+          onClick={() => window?.location.reload()}>
+          {t('message.down_time_retry')}
+        </button>
+        <button
+          type="button"
+          className={styles.viewPrevChatsButton}
+          onClick={() => window?.location.reload()}>
+          {t('message.down_time_view_prev_chats')}
+        </button>
+      </div>
       <Menu />
     </div>
   );
 }
 
 export default DownTimePage;
-
