111489f4f9fa5f7e4cfb4d42626019c8f71309c8
prtkjakhar <prtkjakhar@gmail.com>
2023-08-23 13:47:24 +0530

    bug fix

--- apps/amakrushi/src/utils/textUtility.ts
+++ apps/amakrushi/src/utils/textUtility.ts
@@ -9,7 +9,7 @@ export const getInitialMsgs = (t: any, flags: any, locale: any): any => {
               ? flags?.en_example_ques_one?.value ||
                 "What are the different types of millets grown in Odisha?"
               : flags?.or_example_ques_one?.value ||
-                "ଓଡ଼ିଶାରେ ଉତ୍ପାଦନ ହେଉଥିବା ବିଭିନ୍ନ ପ୍ରକାର ମିଲେଟ୍ ଗୁଡିକ କ'ଣ ?",,
+                "ଓଡ଼ିଶାରେ ଉତ୍ପାଦନ ହେଉଥିବା ବିଭିନ୍ନ ପ୍ରକାର ମିଲେଟ୍ ଗୁଡିକ କ'ଣ ?",
           img: '/example_image_one.png'
         },
         {
@@ -19,7 +19,7 @@ export const getInitialMsgs = (t: any, flags: any, locale: any): any => {
               ? flags?.en_example_ques_two?.value ||
                 "Tell me something about treatment of termites in sugarcane?"
               : flags?.or_example_ques_two?.value ||
-                "ଆଖୁରେ ଟର୍ମାଇଟ୍ସର ଚିକିତ୍ସା ବିଷୟରେ ମୋତେ କିଛି କୁହନ୍ତୁ |",,
+                "ଆଖୁରେ ଟର୍ମାଇଟ୍ସର ଚିକିତ୍ସା ବିଷୟରେ ମୋତେ କିଛି କୁହନ୍ତୁ |",
           img: '/example_image_two.png'
         },
         {
@@ -29,7 +29,7 @@ export const getInitialMsgs = (t: any, flags: any, locale: any): any => {
               ? flags?.en_example_ques_three?.value ||
                 "How can farmers apply to government schemes in Odisha?"
               : flags?.or_example_ques_three?.value ||
-                "କୃଷକମାନେ ଓଡିଶାରେ ସରକାରୀ ଯୋଜନାରେ କିପରି ଆବେଦନ କରିପାରିବେ ?",,
+                "କୃଷକମାନେ ଓଡିଶାରେ ସରକାରୀ ଯୋଜନାରେ କିପରି ଆବେଦନ କରିପାରିବେ ?",
           img: '/example_image_three.png'
         },
       ],
