f508a27a0a3d3f55f43f9d61cc6b527047a613ef
prtkjakhar <prtkjakhar@gmail.com>
2023-09-21 10:36:40 +0530

    pdfurl defined two times fixed

--- apps/amakrushi/src/components/NavBar/index.tsx
+++ apps/amakrushi/src/components/NavBar/index.tsx
@@ -52,8 +52,6 @@ function NavBar() {
       const pdfUrl = response.data.pdfUrl;
       window.open(pdfUrl)
 
-      const pdfUrl = response.data.pdfUrl;
-
       const blob = new Blob([response.data], { type: 'application/pdf' });
       const file = new File([blob], 'Chat.pdf', {type: blob.type});
 
