848938fc2d6eefa2b929429a608714eed014acd3
prtkjakhar <prtkjakhar@gmail.com>
2023-10-09 13:08:58 +0530

    changes for flutter

--- apps/amakrushi/src/components/NavBar/index.tsx
+++ apps/amakrushi/src/components/NavBar/index.tsx
@@ -83,7 +83,7 @@ function NavBar() {
             window.AndroidHandler.shareUrl(pdfUrl);
           }else{
             //@ts-ignore
-            shareUrl(pdfUrl);
+            window.shareUrl(pdfUrl);
           }
         } else if (navigator.canShare({ files: [file] })) {
           toast.success(`${t('message.sharing')}`);
