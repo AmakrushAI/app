7a1694fc276a21b08d69b9ef1d6ab44c75922133
amit-s19 <coutamit.s19@gmail.com>
2024-01-12 15:11:29 +0530

    Fix in record location

--- apps/amakrushi/src/utils/location.ts
+++ apps/amakrushi/src/utils/location.ts
@@ -5,13 +5,24 @@ export async function recordUserLocation() {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(saveUserLocation);
         }
+
         let apiRes: any = await fetch('https://api.ipify.org?format=json');
+
         apiRes = await apiRes.json();
+
         if (apiRes?.ip) {
-            let locationRes: any = await fetch(`https://geoip.samagra.io/city/${apiRes.ip}`);
-            locationRes = await locationRes.json();
-            sessionStorage.setItem('city', locationRes.city);
-            sessionStorage.setItem('state', locationRes.regionName);
+            navigator.permissions.query({ name: 'geolocation' }).then(async (res: any) => {
+
+                let locationRes: any = await fetch(`https://geoip.samagra.io/city/${apiRes.ip}`);
+                locationRes = await locationRes.json();
+                sessionStorage.setItem('city', locationRes.city);
+                sessionStorage.setItem('state', locationRes.regionName);
+
+                if (res.state != 'granted') {
+                    sessionStorage.setItem('latitude', locationRes.lat);
+                    sessionStorage.setItem('longitude', locationRes.lon);
+                }
+            })
         }
     } catch (err) {
         console.log(err)
