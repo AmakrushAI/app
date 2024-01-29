c656819028c7b8f6647d057b6b96693a1bec2795
prtkjakhar <prtkjakhar@gmail.com>
2024-01-29 19:24:51 +0530

    transliteration minor fix

--- packages/chat-ui/lib/components/Composer/ComposerInput.js
+++ packages/chat-ui/lib/components/Composer/ComposerInput.js
@@ -144,7 +144,7 @@ var ComposerInput = function ComposerInput(_ref) {
               },
               serviceId: transliterationConfig.serviceId || 'ai4bharat/indicxlit--cpu-fsv2',
               isSentence: false,
-              numSuggestions: 5
+              numSuggestions: 3
             }
           }],
           inputData: {
@@ -180,14 +180,17 @@ var ComposerInput = function ComposerInput(_ref) {
     if (selectedWord) {
       // Replace the selected word with the transliterated suggestion
       //@ts-ignore
-      var newInputMsg = value.replace(selectedWord, e);
+      var newInputMsg = value.replace(selectedWord,
+      //@ts-ignore
+      cursorPosition === value.length ? e + ' ' : e);
       setSuggestions([]);
       setSuggestionClicked(true);
       setActiveSuggestion(0);
 
       // Save and restore the cursor position
+      var restoredCursorPosition =
       //@ts-ignore
-      var restoredCursorPosition = cursorPosition - value.indexOf(selectedWord) + value.indexOf(e);
+      cursorPosition - value.indexOf(selectedWord) + value.indexOf(e);
       //@ts-ignore
       onChange(newInputMsg, e);
       setCursorPosition(restoredCursorPosition);
