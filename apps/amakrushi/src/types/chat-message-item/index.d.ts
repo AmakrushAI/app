34228b4ac555ca5e31d7bf9f4db8545e5eff5dc4
Amit Sharma <46066481+amit-s19@users.noreply.github.com>
2024-01-31 12:01:00 +0530

    Revert "Revert "Pushing streaming to Prod"" (#218)

--- apps/amakrushi/src/types/chat-message-item/index.d.ts
+++ apps/amakrushi/src/types/chat-message-item/index.d.ts
@@ -1,7 +1,5 @@
-import { UserType } from "../context";
 
 export type ChatMessageItemPropType = {
-  currentUser: UserType;
   message: any;
-  onSend: (text: string, aa: null, a3: boolean, currentUser: UserType) => void;
+  onSend: (text: string, aa: null, a3: boolean) => void;
 };
